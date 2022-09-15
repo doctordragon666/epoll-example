07a5de27a7a90062b4b750ec9a5a62dacb1b0625
DoctorDragon <teacher_jiang03@qq.com>
2022-09-15 04:13:11 -0700

    first commit

---
+++ init.sh
@@ -0,0 +1,7 @@
+echo "# epoll-example" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/doctordragon666/epoll-example.git
+git push -u origin main
